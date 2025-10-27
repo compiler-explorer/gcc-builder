c99e12ec6967f62d0f19dfbedc57ae213826201e
Paul Gey <narpfel@gmx.de>
2025-10-27 19:51:45 +0100

    remove unnecessary workaround

--- build/build-g++-1.27.sh
+++ build/build-g++-1.27.sh
@@ -17,8 +17,6 @@ else
     OUTPUT=${2-/root/g++-${VERSION}.tar.xz}
 fi
 
-# Workaround for Ubuntu builds
-export LIBRARY_PATH=/usr/lib/x86_64-linux-gnu
 STAGING_DIR=/opt/compiler-explorer/g++-${VERSION}
 
 MAJOR=1
