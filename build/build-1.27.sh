83150caf56891e2be6bca8e050dafd7230203c53
Paul Gey <narpfel@gmx.de>
2025-10-28 17:55:53 +0100

    merge g++-1.27 build into gcc-1.27 build script

--- build/build-1.27.sh
+++ build/build-1.27.sh
@@ -17,23 +17,24 @@ else
     OUTPUT=${2-/root/gcc-${VERSION}.tar.xz}
 fi
 
-# Workaround for Ubuntu builds
-export LIBRARY_PATH=/usr/lib/x86_64-linux-gnu
 STAGING_DIR=/opt/compiler-explorer/gcc-${VERSION}
 
 MAJOR=1
 MAJOR_MINOR=1.27
 
-URL=https://gcc.gnu.org/pub/gcc/old-releases/gcc-${MAJOR}/gcc-${MAJOR_MINOR}.tar.bz2
+GCC_URL=https://gcc.gnu.org/pub/gcc/old-releases/gcc-${MAJOR}/gcc-${MAJOR_MINOR}.tar.bz2
+GPLUSPLUS_URL=https://gcc.gnu.org/pub/gcc/old-releases/gcc-${MAJOR}/g++-${MAJOR_MINOR}.0.tar.bz2
 
-curl -L ${URL} | tar jxf -
+# “unload the tapes”
+curl -L ${GCC_URL} | tar jxf -
+curl -L ${GPLUSPLUS_URL} | tar jxf -
 
 applyPatchesAndConfig() {
     local PATCH_DIR=${ROOT}/patches/$1
     local PATCH=""
     if [[ -d ${PATCH_DIR} ]]; then
         echo "Applying patches from ${PATCH_DIR}"
-        pushd gcc-${VERSION}
+        pushd $2
         for PATCH in ${PATCH_DIR}/*; do
             echo "...${PATCH}"
             patch -p1 <${PATCH}
@@ -52,8 +53,8 @@ applyPatchesAndConfig() {
     fi
 }
 
-applyPatchesAndConfig gcc${MAJOR}
-applyPatchesAndConfig gcc${MAJOR_MINOR}
+applyPatchesAndConfig gcc${MAJOR_MINOR} gcc-${MAJOR_MINOR}
+applyPatchesAndConfig g++${MAJOR_MINOR} src-g++
 
 pushd gcc-${MAJOR_MINOR}
 ln -s config-i386v.h config.h
@@ -66,6 +67,25 @@ sed -i "s|^libdir =.*|libdir = /opt/compiler-explorer/gcc-${VERSION}/lib|g" Make
 make -j$(nproc)
 make -j$(nproc) stage1
 make -j$(nproc) CC=stage1/gcc CFLAGS="-Bstage1/ -Iinclude"
+make -j$(nproc) install
+popd
+
+mkdir g++
+
+pushd g++
+ln -s ../src-g++/* .
+ln -s ../gcc-${MAJOR_MINOR}/*.[chy] . || true
+ln -s ../gcc-${MAJOR_MINOR}/*.def . || true
+ln -s ../gcc-${MAJOR_MINOR}/*.md . || true
+ln -s ../gcc-${MAJOR_MINOR}/move-if-change . || true
+ln -s ../gcc-${MAJOR_MINOR}/config-i386v.h config.h || true
+ln -s ../gcc-${MAJOR_MINOR}/tm-i386v.h tm.h || true
+ln -s ../gcc-${MAJOR_MINOR}/i386.md md || true
+ln -s ../gcc-${MAJOR_MINOR}/output-i386.c aux-output.c || true
+
+sed -i "s|^prefix =.*|prefix = /opt/compiler-explorer/gcc-${VERSION}|g" Makefile
+
+make -j$(nproc)
 make -j$(nproc) install
 popd
 
